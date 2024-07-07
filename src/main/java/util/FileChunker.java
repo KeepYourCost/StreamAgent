ddf3b396dc6500737662399f695b9bb8224b760d
Jeong-Rae <kkwjdfo@gmail.com>
2024-07-08 00:54:27 +0900

    [feat] FileChunker 코드 추가

--- src/main/java/util/FileChunker.java
+++ src/main/java/util/FileChunker.java
@@ -1,27 +1,43 @@
-package util;
+    package util;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+    import java.io.IOException;
+    import java.nio.file.Files;
+    import java.nio.file.Paths;
+    import java.util.ArrayList;
+    import java.util.List;
 
-public class FileChunker {
-    private static final int CHUNK_SIZE = 1024;
+    public class FileChunker {
+        public static final int CHUNK_SIZE = 1024;
+        private static volatile FileChunker instance;
 
-    public List<byte[]> splitFileIntoChunks(String filePath) throws IOException {
-        List<byte[]> chunks = new ArrayList<>();
-        byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
-
-        for (int offset = 0; offset < fileBytes.length; offset += CHUNK_SIZE) {
-            int chunkLength = Math.min(CHUNK_SIZE, fileBytes.length - offset);
-            byte[] chunk = new byte[chunkLength];
-
-            System.arraycopy(fileBytes, offset, chunk, 0, chunkLength);
-
-            chunks.add(chunk);
+        private FileChunker() {
         }
 
-        return chunks;
+        public static FileChunker getInstance() {
+            if (instance == null) {
+                synchronized (FileChunker.class) {
+                    if (instance == null) {
+                        instance = new FileChunker();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public List<byte[]> splitFileIntoChunks(String filePath) throws IOException {
+            List<byte[]> chunks = new ArrayList<>();
+            byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
+
+            for (int offset = 0; offset < fileBytes.length; offset += CHUNK_SIZE) {
+                int chunkLength = Math.min(CHUNK_SIZE, fileBytes.length - offset);
+                byte[] chunk = new byte[chunkLength];
+
+                System.arraycopy(fileBytes, offset, chunk, 0, chunkLength);
+
+                chunks.add(chunk);
+            }
+
+            return chunks;
+        }
+
     }
-}
